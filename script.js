dccd650d3bfcb6d899108f2408588fbba189e6b7
Chris-Kerr140 <146110039+Chris-Kerr140@users.noreply.github.com>
2024-07-02 03:46:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-//carousel//
-let slideIndex = 0;
-showSlides();
+//carousel
 
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++){
-        slides[i].computedStyleMap.display = "none";
-    }
-slideIndex++;
-if (slideIndex > slides.length) {
-    slideIndex = 1;
-}
-slides[slideIndex - 1].style.display = "block";
-setTimeout(showSlides,2000);
-}
-
+$(document).ready(function() {
+    $('#videoCarousel').carousel({
+        interval: 3000 // Change the interval if needed
+    });
+});
 //registration form
 
 document.getElementById("registrationForm").addEventListener("submit", function(event) {
