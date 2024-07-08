f259974ecd32b1cfaaa5bf67feb5c97c25cfec49
Chris-Kerr140 <146110039+Chris-Kerr140@users.noreply.github.com>
2024-07-08 15:45:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
-//carousel
-
-$(document).ready(function () {
-    var pos = 0,
-        slides = $('.slide'),
-        numOfSlides = slides.length;
-
-    function nextSlide() {
-        // `[]` returns a vanilla DOM object from a jQuery object/collection
-        slides[pos].video.stopVideo()
-        slides.eq(pos).animate({ left: '-100%' }, 500);
-        pos = (pos >= numOfSlides - 1 ? 0 : ++pos);
-        slides.eq(pos).css({ left: '100%' }).animate({ left: 0 }, 500);
-    }
-
-    function previousSlide() {
-        slides[pos].video.stopVideo()
-        slides.eq(pos).animate({ left: '100%' }, 500);
-        pos = (pos == 0 ? numOfSlides - 1 : --pos);
-        slides.eq(pos).css({ left: '-100%' }).animate({ left: 0 }, 500);
-    }
-
-    $('.left').click(previousSlide);
-    $('.right').click(nextSlide);
-})
-
-function onYouTubeIframeAPIReady() {
-    $('.slide').each(function (index, slide) {
-        // Get the `.video` element inside each `.slide`
-        var iframe = $(slide).find('.video')[0]
-        // Create a new YT.Player from the iFrame, and store it on the `.slide` DOM object
-        slide.video = new YT.Player(iframe)
-    })
-}
-//registration form
-
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-event.preventDefault();
-let email = document.getElementById("email").value;
-let passowrd = document.getElementById("password").value;
-if (email && password) {
-    alert("Registration Successful!");
-} else {
-    alert("Please fill in all fields");
-}
+// Initialize Bootstrap carousel
+$(document).ready(function() {
+    $('#videoCarousel').carousel({
+        interval: 3000 // Change the interval if desired
+    });
 });
 
-// product gallery
+// YouTube API script to stop videos when sliding
+function onYouTubeIframeAPIReady() {
+    var players = [];
+    $('.carousel-item').each(function() {
+        var $this = $(this);
+        var id = $this.find('iframe').attr('id');
+        players[id] = new YT.Player(id);
+    });
 
+    $('#videoCarousel').on('slide.bs.carousel', function(event) {
+        var $currentSlide = $(event.relatedTarget);
+        var $prevSlide = $(event.from);
+        var prevId = $prevSlide.find('iframe').attr('id');
+        if (players[prevId]) {
+            players[prevId].stopVideo();
+        }
+    });
+}
+
+// Load YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Registration form validation
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    if (email && password) {
+        alert("Registration Successful!");
+    } else {
+        alert("Please fill in all fields");
+    }
+});
+
+// Product gallery click event
 let products = document.getElementsByClassName("product");
 for (let product of products) {
     product.addEventListener("click", function() {
-        alert("product selected: " + this.alt);
+        alert("Product selected: " + this.alt);
     });
 }
